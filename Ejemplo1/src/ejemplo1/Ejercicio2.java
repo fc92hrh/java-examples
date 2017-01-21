91473f5fcb93d93a7fcf5e7c928ef1e19397cbbd
Hector Rivera <fc92hrh@gmx.com>
2017-01-21 13:29:23 -0500

    Mi segundo ejercicio checkear la igualdad de dos numeros

---
+++ Ejemplo1/src/ejemplo1/Ejercicio2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo1;
+
+/**
+ *
+ * @author HRH
+ */
+public class Ejercicio2 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double var1 = 1234.678;
+        double var2 = 425.789;
+        if(var1 < var2)
+            System.out.println("Var1 es menor que Var2");
+        else if(var1 > var2)
+            System.out.println("Var1 es mayor que Var2");
+        else
+            System.out.println("Var1 es igual a Var2");
+        
+    }
+}
